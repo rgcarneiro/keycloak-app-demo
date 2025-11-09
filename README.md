6fe81480354aeb098cad3995326be351770e67f5
rgcarneiro <45128877+rgcarneiro@users.noreply.github.com>
2025-11-09 18:21:56 -0300

    first commit: add readme and base file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# keycloak-app-demo
-Very simple keycloak app demonstration for study purposes.
+Requirements:
+1) Docker
+2) Python 3.6+
+
+Run the following command to start Keycloak server:
+
+>docker run -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:26.0.6 start-dev
+
+Configure your Keycloak client and google identity provider.
+Check if credentials are correct in the python files then run the server.
+Add `.env` file to the project with the configuration below.
+
+```env
+HR_OAUTH2_CLIENT_ID=hr_web_app
+HR_OAUTH2_CLIENT_SECRET=""
+CRM_OAUTH2_CLIENT_ID=crm_web_app
+CRM_OAUTH2_CLIENT_SECRET=""
+
+OAUTH2_ISSUER=http://localhost:8080/realms/AuthApp
+FLASK_SECRET=MyLongFlaskSecret
+
+HR_FLASK_PORT=3000
+CRM_FLASK_PORT=3001
+```
---
+++ hr_manager_app/server.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, url_for, session, abort, redirect
+from authlib.integrations.flask_client import OAuth
+import json
+from urllib.parse import quote_plus, urlencode
+
+import os
+from dotenv import load_dotenv
+import pathlib
+
+path = pathlib.Path(__file__).parent.parent.resolve()
+load_dotenv(f"{path}/.env")
+
+app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET")
+
+oauth = OAuth(app)
+oauth.register(
+    "hrApp",
+    client_id=os.getenv("HR_OAUTH2_CLIENT_ID"),
+    client_secret=os.getenv("HR_OAUTH2_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+        "code_challenge_method": "S256"
+    },
+    server_metadata_url=f"{os.getenv('OAUTH2_ISSUER')}/.well-known/openid-configuration",
+)
+
+
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        session=session.get("user"),
+        pretty=json.dumps(session.get("user"), indent=4)
+    )
+
+
+@app.route("/callback")
+def callback():
+    token = oauth.hrApp.authorize_access_token()
+    session["user"] = token
+
+    return render_template("ponto.html")
+
+
+@app.route("/login")
+def login():
+    if "user" in session:
+        abort(404)
+    return oauth.hrApp.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+
+@app.route("/logout")
+def logout():
+    id_token = session["user"]["id_token"]
+    session.clear()
+
+    return redirect(os.getenv("OAUTH2_ISSUER")
+                    + "/protocol/openid-connect/logout?"
+                    + urlencode(
+                        {
+                            "post_logout_redirect_uri": url_for("loggedout", _external=True),
+                            "id_token_hint": id_token
+                        },
+                        quote_via=quote_plus,
+    )
+    )
+
+
+@app.route("/loggedout")
+def loggedout():
+    if "user" in session:
+        abort(404)
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=os.getenv("HR_FLASK_PORT"), debug=True)
